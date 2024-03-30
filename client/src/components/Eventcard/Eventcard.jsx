e852f79620c40eba3870d37fd982e88a03e7f66b
Soumyajit <sona832004@gmail.com>
2024-03-30 19:41:42 +0530

    [PULL] pulling after making Eventcard as component

--- client/src/components/Eventcard/Eventcard.jsx
+++ client/src/components/Eventcard/Eventcard.jsx
@@ -1,65 +1,34 @@
 import {React, useState} from "react";
 import "./Eventcard.css"
 
-const Eventcard = ({ controlProp }) => {
+const Eventcard = ({ controlProp, Eventdata, FocusEvent }) => {
   const [expand, setexpand] = useState(0);
   const handleExpand = (idx) => {
     if(expand===idx)setexpand(null);
     else setexpand(idx);
   };
 
-  const allEvents = [
-    {
-      eventname: "SATURNALIA",
-      eventimage: "ashtami",
-    },
-    {
-      eventname: "SATURNALIA",
-      eventimage: "ashtami",
-    },
-    {
-      eventname: "SATURNALIA",
-      eventimage: "ashtami",
-    },
-    {
-      eventname: "SATURNALIA",
-      eventimage: "ashtami",
-    },
-    {
-      eventname: "SATURNALIA",
-      eventimage: "ashtami",
-    },
-    {
-      eventname: "SATURNALIA",
-      eventimage: "ashtami",
-    },
-    {
-      eventname: "SATURNALIA",
-      eventimage: "ashtami",
-    },
-  ];
-
   const majorEvents = [0, 3, 6];
 
   return (
     <div style = {controlProp} className="event-card">
-      {allEvents.map((value, index) => {
+      {Eventdata.map((value, index) => {
         var show = expand === index ? true : false;
-        if (majorEvents.includes(index)) {
+        if (FocusEvent.includes(index)) {
           show = true;
         }
         return (
           <div key={index} className="event-data">
             <div onClick={() => handleExpand(index)} className="data-header">
-              {value.eventname}
-              <p>11:30</p>
+              {value.eventName}
+              <p>{value.time}</p>
             </div>
             <div className="data-body">
               <img
                 style={{
                   width: show ? "22rem" : "0",
                 }}
-                src={`/assets/imgs/home/${value.eventimage}.png`}
+                src={`/assets/imgs/home/${value.eventName}.png`}
                 alt=""
               />
             </div>
--- client/src/screens/Daydetails/Daydetails.jsx
+++ client/src/screens/Daydetails/Daydetails.jsx
@@ -3,6 +3,9 @@ import "./Daydetails.css";
 
 import Eventcard from "../../components/Eventcard/Eventcard";
 
+import Rebeca from "../../../public/assets/eventDetails.json";
+import PujaHeading from "../../components/PujaHeading/PujaHeading";
+
 const Artist = ({ artistName }) => {
   return (
     <div className="artist-banner">
@@ -13,42 +16,35 @@ const Artist = ({ artistName }) => {
 };
 
 const Daydetails = () => {
+  const Day = Rebeca.saptami;
 
   const [controlMargin, setControlMargin] = useState(0);
-  const handleMargin = (direction) =>{
-    
-    if(direction===1){
-      if(controlMargin===0)return;
+  const handleMargin = (direction) => {
+    if (direction === 1) {
+      if (controlMargin === 0) return;
 
       setControlMargin(controlMargin + 10);
-    }
-    else{
-      if(controlMargin<=-100)return;
+    } else {
+      if (controlMargin <= -100) return;
       setControlMargin(controlMargin - 10);
     }
-  }
+  };
   return (
     <div className="day-details-wrapper">
       <div className="behind-banner"></div>
-      <div className="section-head">
+      {/* <div className="section-head">
         <div className="full-date">
-          <div>20</div>
+          <div>{Day.date}</div>
           <p>April</p>
         </div>
-        <div className="day-nickname">Saptami</div>
-      </div>
-      <div className="section-1">
-        Get ready to soak in the rich heritage of Indian culture as talented artists from all across the nation weave
-        together a tapestry of grace and timeless magic, filling the air with the tunes of celebration and marking the
-        very beginning of our very own Pujo
-      </div>
+        <div className="day-nickname">{"Saptami"}</div>
+      </div> */}
+      <PujaHeading date={20} datetxt={"SAPTAMI"} />
+      <div className="section-1">{Day.intro}</div>
 
       <div className="section-2">
         <div className="section-subhead">Significance</div>
-        Get ready to soak in the rich heritage of Indian culture as talented artists from all across the nation weave
-        together a tapestry of grace and timeless magic, filling the air with the tunes of celebration and marking the
-        very beginning of our very own Pujo. filling the air with the tunes of celebration and marking the very
-        beginning of our very own Pujo.
+        {Day.significance}
       </div>
       <div className="section-3">
         <div className="hatchline-banner"></div>
@@ -56,22 +52,30 @@ const Daydetails = () => {
         Brace Yourself for an Unforgettable Showcase, As We Proudly Unveil Our Stellar Lineup
       </div>
       <div className="section-4">
-        <Artist artistName={"artist1"} />
-        <Artist artistName={"artist3"} />
-        <Artist artistName={"artist2"} />
-        <Artist artistName={"artist3"} />
+
+        {Day.lineUps.map((value, index) => {
+          return <Artist artistName={value} key={index} />;
+        })}
       </div>
 
       <div className="section-5">
         <div className="section-subhead">Events</div>
       </div>
       <div className="section-event">
-        <Eventcard controlProp = {{
-          marginLeft: `${controlMargin}rem`
-        }} />
+        <Eventcard
+          controlProp={{
+            marginLeft: `${controlMargin}rem`,
+          }}
+          Eventdata = {Day.eventList}
+          FocusEvent = {Day.majorEvents}
+        />
         <div className="event-controller">
-          <div className="controller-btn material-icons" onClick = {()=>handleMargin(1)}>arrow_back</div>
-          <div className="controller-btn material-icons"onClick = {()=>handleMargin(0)}>arrow_forward</div>
+          <div className="controller-btn material-icons" onClick={() => handleMargin(1)}>
+            arrow_back
+          </div>
+          <div className="controller-btn material-icons" onClick={() => handleMargin(0)}>
+            arrow_forward
+          </div>
         </div>
       </div>
     </div>
