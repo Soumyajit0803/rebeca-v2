62c541f07b1d09051ac19003f73e8e66c322dad0
Soumyajit <sona832004@gmail.com>
2024-04-04 02:52:26 +0530

    [MERGE]: merging JSON data updates

--- client/src/components/Eventcard/Eventcard.jsx
+++ client/src/components/Eventcard/Eventcard.jsx
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from "react";
 import "./Eventcard.css";
 import { Link } from "react-router-dom";
 
-const Eventpanel = ({ value, index, FocusEvent }) => {
+const Eventpanel = ({ value, index, day, FocusEvent }) => {
   const [expand, setexpand] = useState(0);
   const handleExpand = (idx) => {
     if (expand === idx) setexpand(null);
@@ -17,44 +17,37 @@ const Eventpanel = ({ value, index, FocusEvent }) => {
   const propsWidth = { width: show ? "22rem" : "0px" };
 
   return (
-		<div key={index} className="event-data">
-			<div onClick={() => handleExpand(index)} className="data-header">
-				{value.eventName}
-				<p>{value.time}</p>
-			</div>
+    <div key={index} className="event-data">
+      <div onClick={() => handleExpand(index)} className="data-header">
+        <div>{value.eventName}</div>
+        <p>{value.time}</p>
+      </div>
 
-			<div className="data-body">
-				<Link to={`/event/${value.eventName}`}>
-					<div
-						className="img"
-						style={{
-							display:
-								window.innerWidth > 500 || show
-									? "contents"
-									: "none",
-						}}
-					>
-						<img
-							style={
-								window.innerWidth <= 500
-									? propsHeight
-									: propsWidth
-							}
-							src={`/assets/imgs/home/${value.eventName.slice(0, -3)}.webp`}
-							alt="event-icon"
-						/>
-					</div>
-				</Link>
-			</div>
-		</div>
+      <div className="data-body">
+        <Link to={`/event/${value.eventName}`}>
+          <div
+            className="img"
+            style={{
+              display: window.innerWidth > 500 || show ? "contents" : "none",
+            }}
+          >
+            <img
+              style={window.innerWidth <= 500 ? propsHeight : propsWidth}
+              src={`/assets/imgs/home/${day}.webp`}
+              alt="event-icon"
+            />
+          </div>
+        </Link>
+      </div>
+    </div>
   );
 };
 
-const Eventcard = ({ Eventdata, FocusEvent }) => {
+const Eventcard = ({ Eventdata, FocusEvent, Eventday }) => {
   return (
     <div className="event-card">
       {Eventdata.map((value, index) => (
-        <Eventpanel key={index} value={value} index={index} FocusEvent={FocusEvent} />
+        <Eventpanel key={index} value={value} day={Eventday} index={index} FocusEvent={FocusEvent} />
       ))}
     </div>
   );
