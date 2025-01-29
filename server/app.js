3a6196315b1a6641f074897d1c76122be892ec2c
Abhijit <artistrup07@gmail.com>
2025-01-29 07:28:19 +0530

    [BUILD]: Member adding routes

--- server/app.js
+++ server/app.js
@@ -58,10 +58,12 @@ app.use(express.urlencoded({ extended: true, limit: '100mb' })); // <- Parses UR
 const router = require('./routes/mainroutes');
 const authRouter = require('./routes/authRoutes');
 const adminRouter = require('./routes/adminRoutes')
+const memberRouter = require('./routes/memberRoutes')
 
 app.use('/api/v1/', router); // <- Calling the router
 app.use('/api/v1/auth/', authRouter);
 app.use('/api/v1/admin/', adminRouter);
+app.use('/api/v1/member/', memberRouter)
 
 
 // app.all('*', (req, res, next) => {	// <- Middleware to handle Non-existing Routes
--- server/controllers/memberController.js
+++ server/controllers/memberController.js
@@ -1,4 +1,16 @@
-const axios = require('axios')
-const catchAsync = require('../utils/catchAsync')
-const Member = require('../models/memberModel')
+const axios = require("axios");
+const catchAsync = require("../utils/catchAsync");
+const Member = require("../models/memberModel");
 
+exports.createMember = catchAsync(async (req, res, next) => {
+    try {
+        const memberData = new Member({
+            ...req.body,
+        });
+        await memberData.save();
+        return res.json({ message: "success", data: memberData });
+    } catch (err) {
+        console.log("Error while adding member" + ` message: ${err.message}`);
+        next(err)
+    }
+});
