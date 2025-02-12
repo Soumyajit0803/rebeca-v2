d200664e250e961f81de0fb8b9b278598cda25d6
Soumyajit <sona832004@gmail.com>
2025-02-13 00:28:10 +0530

    [UPDATE]: Building an avatar for showing user

--- admin/src/Components/Dashboard/Dashboard.jsx
+++ admin/src/Components/Dashboard/Dashboard.jsx
@@ -1,10 +1,38 @@
 import React, { useState } from "react";
-import { Tabs, message } from "antd";
+import { Avatar, Tabs, message, Menu, Typography, Dropdown } from "antd";
 import EventRegistration from "../EventAddition/EventAddition";
 import MemberAddition from "../MemberAddition/MemberAddition";
 import MemberEditing from "../MemberEditing/MemberEditing";
 import RegistrationStats from "../RegistrationStats/RegistrationStats";
 import EventEditing from "../EventEditing/EventEditing";
+
+const AvatarMenu = ({ user, handleLogout }) => {
+    const menuItems = [
+        {
+            key: "welcome",
+            label: (
+                <>
+                    <Typography.Text strong>{user.name.split(" ")[0]}</Typography.Text>
+                    <br />
+                    <Typography.Text>Admin</Typography.Text>
+                </>
+            ),
+            disabled: true, // Prevents clickability
+        },
+        { type: "divider" },
+        {
+            key: "logout",
+            label: "Logout",
+            onClick: handleLogout,
+        },
+    ];
+
+    return (
+        <Dropdown menu={{ items: menuItems }} trigger={["click"]} placement="bottomRight">
+            <Avatar src={user.image} alt={user.name} size={50} style={{ cursor: "pointer" }} />
+        </Dropdown>
+    );
+};
 
 const Dashboard = () => {
     const [OpSelect, setOpSelect] = useState(1);
@@ -79,8 +107,24 @@ const Dashboard = () => {
         <>
             {contextHolder}
             <div className="app-wrapper">
-                <div style={{ fontSize: "2.5rem", fontWeight: "600", padding: "2rem", color: "#fff" }}>
-                    Admin Dashboard
+                <div
+                    className="header"
+                    style={{
+                        display: "flex",
+                        // alignItems: "center",
+                        justifyContent: "space-between",
+                        padding: "2rem",
+                    }}
+                >
+                    <div style={{ fontSize: "2.5rem", fontWeight: "600", color: "#fff" }}>Admin Dashboard</div>
+                    <div
+                        className="avatar"
+                        style={{
+                            width: "60",
+                        }}
+                    >
+                        <AvatarMenu user={{ name: "hello" }} />
+                    </div>
                 </div>
                 <Tabs
                     defaultActiveKey="1"
