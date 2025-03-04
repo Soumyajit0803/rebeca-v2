f3ab295400a874c3221a74f0e002eb1331f90056
Abhijit <artistrup07@gmail.com>
2025-03-05 00:10:07 +0530

    [UPDATE]: Reducing type of users to ONLY ONE with optional parameters to signify role (admin/normal user)

---
+++ server/controllers/userController.js
@@ -0,0 +1,66 @@
+const axios = require("axios");
+const catchAsync = require("../utils/catchAsync");
+const User = require("../models/userModel");
+
+exports.createUser = catchAsync(async (req, res, next) => {
+    try {
+        console.log(req.body);
+        
+        const userData = new User({
+            ...req.body,
+        });
+        await userData.save();
+        return res.status(201).json({ message: "success", data: userData });
+    } catch (err) {
+        console.log("Error while adding user" + ` message: ${err.message}`);
+        next(err)
+    }
+});
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+    try {
+        const allUsers = await User.find()
+        return res.status(200).json({message: "success", data: allUsers})
+
+    } catch(err) {
+        console.log(err.message);
+        next(err)
+    }
+})
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+    
+    try {
+        console.log(req.body);
+        
+        const updatedUser = await User.findByIdAndUpdate(
+            req.body._id,
+            {$set: req.body},
+            {new: true, runValidators: true}
+        )
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User ID not found"});
+        }
+
+        return res.status(200).json({message: 'success', data: updatedUser})
+    } catch(err) {
+        console.log(err.message);
+        next(err)
+    }
+})
+
+exports.deleteUser = catchAsync(async (req, res, next) => {
+    try {
+        const id = req.query._id
+        const deletedUser = await User.findByIdAndDelete(id)
+        if (!deletedUser) {
+            return res.status(404).json({message: "User to be deleted not found"})
+        } else{
+            return res.status(204).json({message: "success"})
+        }
+
+    } catch(err) {
+        next(err)
+    }
+})
